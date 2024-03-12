b7707f0f67b48f5c11a3a8e00507b79231067ebe
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-03-13 00:12:25 +0100

    add auth stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import db from "@astrojs/db";
+import db from "@astrojs/db"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import { defineConfig } from "astro/config"
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db()],
-  output: "server",
-  adapter: cloudflare()
-});
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), db()],
+	output: "server",
+	adapter: cloudflare(),
+	vite: {
+		optimizeDeps: {
+			exclude: ["oslo"],
+		},
+	},
+})
